0af478472e531d9ea6b6eecca8a5448faff26a63
lamboyjat <lamboyjat@gmail.com>
2021-05-02 20:29:37 +0200

    changing the Banner insertion to DB and PascalCase on name

--- banner.php
+++ banner.php
@@ -255,13 +255,7 @@ class Banner extends Module
         foreach (array_keys($form_values) as $key) {
             Configuration::updateValue($key, Tools::getValue($key));
         }
-
-        if (isset($params[$form_values]) && $params[$form_values] != '') {
-            Db::getInstance()->execute(
-                "INSERT INTO `"._DB_PREFIX_."multi_banner` (`id_multi_banner`, `color`, 
-                `background_color`, `content`, `start_date`, `end_date`, `active`, `priority`) 
-            VALUES (".intval($Banner->id).", 0, 0, '".$_POST['text']."', 0, NOW())");
-        }
+        
     }
 
     /**
